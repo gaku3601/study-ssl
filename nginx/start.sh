fd5e12d3907f72229f29ee39246c70abc3250d86
gaku3601 <t_hidaka@takuto-net.com>
2018-02-08 06:37:29 +0900

    リバースプロキシがかかるように実装

--- nginx/start.sh
+++ nginx/start.sh
@@ -2,8 +2,18 @@ if [ -e /etc/letsencrypt/live ]; then
     echo "SSL Certificate Folder is Fonund"
 else
     echo "SSL Certificate Folder is Not Fonund"
-    certbot certonly --test-cert --webroot --webroot-path /usr/share/nginx/html/ssl -d takuto.gakusmemo.com -m pro.gaku@gmail.com --agree-tos -n
+    # 証明書の発行
+    if [ $STAGE = "production" ]; then
+        echo "This is Production" 
+        certbot certonly --webroot --webroot-path /usr/share/nginx/html/ssl -d $DOMAIN -m $MAIL --agree-tos -n
+    else
+        echo "This is Staging" 
+        certbot certonly --test-cert --webroot --webroot-path /usr/share/nginx/html/ssl -d $DOMAIN -m $MAIL --agree-tos -n
+    fi
+    # nginxの設定
     mv /etc/nginx/conf.d/default.ssl.conf~ /etc/nginx/conf.d/default.ssl.conf
+    sed -i -e "s/convert_domain/$DOMAIN/g" /etc/nginx/conf.d/default.ssl.conf
+    sed -i -e "s|convert_pass|$REVERSE_PROXY|g" /etc/nginx/conf.d/default.ssl.conf
     supervisorctl restart nginx
 fi
 
